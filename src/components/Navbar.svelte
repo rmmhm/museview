83e1228e85e2842c116c4c1e6696fa0fb2967006
Ryan <ryanma22303@gmail.com>
2023-05-06 21:25:32 -0400

    oh man

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,36 @@
+
+
+<style>
+    nav {
+        display: flex;
+        position: fixed;
+        align-items: center;
+        justify-content: space-between;
+        background-color: teal;
+        min-width: 100%;
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+    }
+
+    li {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+</style>
+
+<nav>
+    <ul>
+        <li>
+            <button>
+                Login
+            </button>
+            <button>
+                Signup
+            </button>
+        </li>
+    </ul>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit
-	<a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+	import Navbar from '../components/Navbar.svelte';
+</script>
+
+<style>
+	:global(body) {
+		font-family: Helvetica, sans-serif;
+	}
+	h1 {
+		text-align: center;
+	}
+</style>
+
+<Navbar />
+<h1>
+	Welcome to Museview
+</h1>
